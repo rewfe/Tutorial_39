340233b65a8f782f79cfdb8460d1b8b9f6a1f792
rewfe <qrewfe@yandex.ru>
2015-06-27 02:20:47 +0300

    Initial Commit

---
+++ Tutorial_39/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tutorial_39
+//
+//  Created by Admin on 27.06.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tutorial_39/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tutorial_39
+//
+//  Created by Admin on 27.06.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
